2c5847f085c77a3ffe3d7bc23825374ad57a1a47
jins <76993633+jinhyojin@users.noreply.github.com>
2024-12-06 17:48:37 +0900

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ApexHeatmapChart from "./components/ApexHeatmapChartComponent";
 
 const App = () => {
 
-    const API_BASE_URL =  'http://133.186.250.55:32032';
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ;
     const chartSeries = []; // realtime chart data
     const chartData1 = [];  // line 데이터 1
     const chartData2 = [];  // line 데이터 2
